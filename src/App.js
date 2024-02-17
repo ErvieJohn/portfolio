a177ed2d5db6368151758f7f6fa19f7e460f0626
erviejohn <erviejohn.villareal79@gmail.com>
2024-02-17 13:00:07 +0800

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -7,20 +7,15 @@ import {
   Route,
 } from "react-router-dom";
 import Home from './components/Home/Home';
-import About from './components/About/About';
 import Error from './components/Error/Error';
-import Layout from './layout/Layout';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route element={<Layout />} >
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/*" element={<Error />} />
-          </Route>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/*" element={<Error />} />
         </Routes>
       </Router>
     </div>
